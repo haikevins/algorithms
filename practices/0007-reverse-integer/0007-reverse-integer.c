1b2efa170b94a9752bce02782af818054d4273a6
haikevins <nguyengochai2004@gmail.com>
2026-07-06 09:05:40 +0700

    Time: 0 ms (Beats: 100.00%), Space: 9.27 MB (Beats: 49.38%) - LeetCode

---
+++ practices/0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,29 @@
+#include <limits.h>
+
+int reverse(int x) 
+{
+    int result = 0;
+
+    while (x != 0) 
+    {
+        int digit = x % 10;
+        x = x / 10;
+
+        /*
+            Check overflow and underflow conditions before updating the result.
+        */
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > 7)) 
+        {
+            return 0;
+        }
+
+        if (result < INT_MIN / 10 || (result == INT_MIN / 10 && digit < -8)) 
+        {
+            return 0;
+        }
+
+        result = result * 10 + digit;
+    }
+
+    return result;
+}

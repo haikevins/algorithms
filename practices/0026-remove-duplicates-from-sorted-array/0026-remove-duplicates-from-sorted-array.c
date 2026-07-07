36753687742e1e5bed05eb472cdf241eec2946e3
haikevins <nguyengochai2004@gmail.com>
2026-07-07 16:08:20 +0700

    Time: 0 ms (100.00%), Space: 12.94 MB (41.22%) - LeetCode

---
+++ practices/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,20 @@
+int removeDuplicates(int* nums, int numsSize) 
+{
+    if (numsSize == 0) 
+    {
+        return 0;
+    }
+
+    int uniqueCount = 1;
+
+    for (int i = 1; i < numsSize; i++) 
+    {
+        if (nums[i] != nums[i - 1]) 
+        {
+            nums[uniqueCount] = nums[i];
+            uniqueCount++;
+        }
+    }
+
+    return uniqueCount;    
+}
